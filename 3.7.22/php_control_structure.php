a1e5f6932b814551790ac1753d3d68a427d9a7bf
Saiful Islam <codersaiful@gmail.com>
2022-07-03 20:15:00 +0600

    note

--- 3.7.22/php_control_structure.php
+++ 3.7.22/php_control_structure.php
@@ -5,5 +5,7 @@
  * for,white,do-while,foreach
  * break,continue,goto
  * 
+ * @link https://www.php.net/manual/en/language.control-structures.php
+ * @author Saiful Islam <codersaiful@gmail.com>
  * 
  */
