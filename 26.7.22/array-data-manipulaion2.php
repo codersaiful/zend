affe4f7de3826bcab575f0b4bc3f19e2cec68290
Saiful Islam <codersaiful@gmail.com>
2022-07-26 20:49:54 +0600

    running

--- 26.7.22/array-data-manipulaion2.php
+++ 26.7.22/array-data-manipulaion2.php
@@ -3,3 +3,11 @@
  * Array Filter
  * array_filter()
  */
+
+ $arr = ['a'=>'A','b'=>'B'];
+ $filArr = array_filter($arr,function($k){
+        var_dump($k);
+        return true;
+     },ARRAY_FILTER_USE_KEY);
+
+ var_dump($filArr);
