9e8e5918960ed31adcbf35a63c0d3842f2d63353
Saiful Islam <codersaiful@gmail.com>
2022-07-30 10:45:30 +0600

    working

---
+++ practice-random/test.php
@@ -0,0 +1,13 @@
+<? echo "Hello"; //short tag // Need short_open_tag enabled ?>
+<?= "Hello" //short echo tag ?>
+<hr>
+<?php 
+echo "Welcome","To","World";
+echo 1,2,3;
+echo '<hr>';
+/**
+ * if start by 0, it consider as octal
+ * if 0x, it Hexa Decimal
+ * if 0b, it Binary
+ */
+echo 0b101;
