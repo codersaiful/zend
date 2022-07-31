339983d82d7c07227ba9d18af23fc66c4921123e
Saiful Islam <codersaiful@gmail.com>
2022-07-31 20:47:18 +0600

    class running

---
+++ 37.7.22/array_diffrent.php
@@ -0,0 +1,58 @@
+<?php 
+/**
+ * array_walk and array_warl_recursive()
+ * egulo valu return kore na.
+ */
+
+//  array_reduce()
+//bises kore sum korar jonno
+
+$test_var = range(2,50);
+var_dump(array_reduce($test_var,function($new,$item){
+    return $new+=$item;
+}));//array_reduct() a third params a default value dite hoy.
+
+/**
+ * array_chunk() -> খন্ড 
+ * index mul array moto rakhte chaile third a tru dite hobe
+ * 
+ */
+
+
+//  array_slice() actual index dorkar hole last param a tru dite hobe
+
+/**
+ * array_column()
+ */
+$students = [
+    ['name' => 'Mamun', 'age' => 8, 'address' => 'Dhaka, Bangladesh'],
+    ['name' => 'Muhib', 'age' => 7, 'address' => 'Savar, Bangladesh'],
+    ['name' => 'Mahmud', 'age' => 4, 'address' => 'Rajsahi, Bangladesh'],
+   ];
+
+   var_dump(array_column($students,'name','age'));
+
+/**
+ * array_combine()
+ * 
+ * doto array jog kora, prothom ta key, ekta value hobe. 
+ */
+
+ /**
+  * array_merge()
+
+  array_merge_recursive()
+  */
+
+  /**
+   * array_push() last a value add kore
+   * array_pop() last value bad dicche
+   * 
+   * array_unshift() prothome data dhukabe
+   * array_shift() prothom value remove korbe
+   * mull array ke poriborton kore
+   * return value thake na, tai variable er modhe nite hoyna
+   * 
+   */
+
+   
