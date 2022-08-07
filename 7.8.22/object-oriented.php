aa02fa3f210c85d1c2016cf8e424daae8cac3fa4
Saiful Islam <codersaiful@gmail.com>
2022-08-07 20:24:55 +0600

    1111

--- 7.8.22/object-oriented.php
+++ 7.8.22/object-oriented.php
@@ -5,7 +5,12 @@
  * 1.1 OOP Concept
  *      a. Inheritance
  *      b. Encapsulation
+ *          1.Visibility/Access Modifier
+ *          --public
+ *          --protected
+ *          --private
  *      c. Polymorphism
+ *          
  * 
  * 3. What is Class
  * 3. What is Object
