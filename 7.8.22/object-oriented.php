d470849d673053da81d50eb355ace1c41dd4c6ea
Saiful Islam <codersaiful@gmail.com>
2022-08-07 20:18:29 +0600

    class running

--- 7.8.22/object-oriented.php
+++ 7.8.22/object-oriented.php
@@ -1,1 +1,14 @@
 <?php 
+/**
+ * List of OOP/ OOP concept
+ * 1. What is OOP
+ * 1.1 OOP Concept
+ *      a. Inheritance
+ *      b. Encapsulation
+ *      c. Polymorphism
+ * 
+ * 3. What is Class
+ * 3. What is Object
+ * 
+ * 
+ */
