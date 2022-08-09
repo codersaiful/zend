4c00c5431800131ea68730d99f539a9bc0df266b
Saiful Islam <codersaiful@gmail.com>
2022-08-09 21:06:27 +0600

    Question finished

---
+++ test/question-answer.php
@@ -0,0 +1,77 @@
+MD. Saiful Islam
+drive link: https://drive.google.com/drive/folders/188PtMhKUxf_khwRReFdQJFwK9ehC6Rwj
+
+Q1: B,C, E 
+
+Q2: B
+
+Q3: C
+
+Q4: 
+
+Q5:
+
+Q6: B
+
+Q7: A 
+
+Q8: A
+
+Q9: A
+
+Q10: E
+
+Q11: D 
+
+Q12: E ...
+
+Q13: B 
+
+Q14: B
+
+Q15: D 
+
+Q16: D 
+
+Q17: 1 
+
+Q18: 1
+
+Q19: 2 and 3
+
+Q20: ??
+
+Q21: B  ...
+
+Q22: E 
+
+Q23: C
+
+Q24: B
+
+Q25: B 
+
+Q26: B
+
+Q27: B
+
+Q28: B
+
+Q29: B 
+30: ... 
+31: D 
+32: C 
+33: ... 
+34: F 
+36: D 
+37: D 
+38: A 
+39: D 
+40: A 
+41: 2
+
+
+
+
+
+
