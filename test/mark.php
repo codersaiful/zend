5e48d08c4afea297640bdcf4dc3f7b6f953d835d
Saiful Islam <codersaiful@gmail.com>
2022-08-17 21:53:11 +0600

    practicing

---
+++ test/mark.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    var_dump($_POST);
+    ?>
+    <form action="" method="post">
+        <select name="form" id="nothing">
+            <option value="">Saiful</option>
+            <option>Saiful2</option>
+            <option>Saiful23</option>
+            <option>Saiful234</option>
+        </select>
+        <input type="submit" value="submit">
+    </form>
+</body>
+</html>
--- test/practice2.php
+++ test/practice2.php
@@ -8,4 +8,31 @@ var_dump("Output:",count($a),$a);
 echo "<hr>";
 $string = "This is a String";
 // echo $string[2];
-echo $string{2};
+//echo $string{2}; //From 7.4 version, curly bracket is no longer available in php
+
+echo '<hr>';
+
+function increment(&$val){
+    $val += 1;
+}
+$a = 2;
+increment($a);
+increment($a);
+increment($a);
+increment($a);
+var_dump($a);
+increment($a);
+increment($a);
+var_dump($a);
+echo '<hr>';
+function test_inc(){
+    static $num = 0;
+    return $num+=1;
+}
+
+var_dump(test_inc());
+var_dump(test_inc());
+var_dump(test_inc());
+var_dump(test_inc());
+var_dump(test_inc());
+var_dump(test_inc());
