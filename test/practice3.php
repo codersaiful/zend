cc526bc82308d2c112979b477ed10240c2607546
Saiful Islam <codersaiful@gmail.com>
2022-08-17 23:34:47 +0600

    S

--- test/practice3.php
+++ test/practice3.php
@@ -1,1 +1,18 @@
 <?php 
+
+$saiful = function($val){
+
+    return $val;
+};
+
+var_dump($saiful("Param Stringdddddddddddddddddddddddddddddddddddddddddddddd"));
+
+function my_func($numb){
+    $output = function($in_numb) use ($numb){
+        return $numb . " " . $in_numb;
+    };
+    return $output;
+}
+$ssss = my_func("Saiful");
+var_dump($ssss);
+echo $ssss("Islam");
