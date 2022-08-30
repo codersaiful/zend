d67f631f7dba72ddf98509f704d0d5606008d9b7
Saiful Islam <codersaiful@gmail.com>
2022-08-30 21:26:40 +0600

    class ses

--- 30.8.22/mock-solution.php
+++ 30.8.22/mock-solution.php
@@ -23,3 +23,33 @@ function magic($p, $q) {
    var_dump(magic(3,4));
    $a = `ls -l`;
 var_dump($a);
+switch(1) {
+    case 1: print("Book Details");
+    case 2: print("Book Author");
+    default: print("Missing Book");
+   }
+
+var_dump('--');
+$x = 0;
+$i;
+for($i = 0; $i < 5; $i++) {
+    var_dump($i);
+ $x += $i;
+}
+print($x);
+/**
+ * constand decline korar arekta niyom.
+ */
+const a = "hello";
+const A = "Hello";
+var_dump('--');
+$a = 20;
+function myfunction($b) {
+ $a = 30;
+ global $a, $c;
+ return $c = ($b + $a);
+}
+print myfunction(40) + $c;
+echo '<br>';
+$a = 20 % -8;
+echo $a;
