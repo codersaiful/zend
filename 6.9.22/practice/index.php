afe66c5dc6eb18a7cd4f11f335c9fef5b702b56f
Saiful Islam <codersaiful@gmail.com>
2022-09-08 09:27:14 +0600

    practicing

--- 6.9.22/practice/index.php
+++ 6.9.22/practice/index.php
@@ -3,7 +3,8 @@ namespace Saiful;
 
 use Saiful\App\DB;
 
-require 'app/db.php';
+// require 'app/db.php';
+require 'vendor/autoload.php';
 $db = DB::getInstance();
 $users = $db->query( "SELECT * FROM users" ); // WHERE ID = 5 //  WHERE ID = 90
 $all_fetch = $db->fetchAll($users);
