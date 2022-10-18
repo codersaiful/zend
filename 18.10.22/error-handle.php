fbe4db2beaf442f0e713c727e5e206a4aa672146
Saiful Islam <codersaiful@gmail.com>
2022-10-18 20:46:10 +0600

    file updated

---
+++ 18.10.22/error-handle.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+ * Exception Handle
+ * 
+ */
+class CustomException extends Exception{
+    public function getErrors(){
+        var_dump($this);
+    }
+}
+//  error_reporting(0);//0 dile kchu dekhabena.
+function checkNum($num){
+    if($num <= 0){
+        throw new Exception("Please insert Getter Number");
+        // throw new CustomException("Please insert Getter Number");
+    }else{
+        return $num;
+    }
+}
+
+echo checkNum(0);
+
+try{
+    echo checkNum(0);
+} catch(CustomException $e){
+    $e->getErrors();
+    // echo $e->getMessage();
+    // var_dump($e);
+}
