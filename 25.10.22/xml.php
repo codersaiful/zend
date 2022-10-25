634054142ec0606d5728c5986ad3cec06d83e171
Saiful Islam <codersaiful@gmail.com>
2022-10-25 20:22:27 +0600

    working

--- 25.10.22/xml.php
+++ 25.10.22/xml.php
@@ -1,4 +1,15 @@
 <?php 
 /**
+ * XML - extensible markup language
+ * for store, carry and transport data
+ * Released: 1990 First released
+ * Recommendation by W3C at 10 February, 1998
  * 
+ * Platform: Independent - It's work with all language
+ * 
+ * It's only one version
+ * 
+ * Builtin Extension:
+ * SimpleXML  https://www.php.net/manual/en/book.simplexml.php
+ * PHP DOMDocument https://www.php.net/manual/en/class.domdocument.php
  */
