53e55c3e19bce377168e6b4741d4eb52fb633a4e
Saiful Islam <codersaiful@gmail.com>
2022-11-01 19:29:12 +0600

    practising

--- 30.10.22/edit-xml.php
+++ 30.10.22/edit-xml.php
@@ -1,6 +1,22 @@
 <?php 
 
+// $sxe = simplexml_load_file('data-others-another.xml');
+// $rand_name = 'xml' . rand(78, 787878);
+// $rand_value = 'value:' . rand(9999, 9787878);
+// $total_items = count($sxe->student);
+// $rand_index = rand(0,($total_items-1));
+// var_dump($rand_index);
+// $sxe->student[$rand_index]->addChild($rand_name,$rand_value);
+// $sxe->asXML('data-others-another.xml');
+// var_dump($sxe);
+
+
 $sxe = simplexml_load_file('data.xml');
-$sxe->student->addChild('fathar-name','Rezaul Haque');
-$sxe->asXML('data-others.xml');
+$rand_name = 'xml' . rand(78, 787878);
+$rand_value = 'value:' . rand(9999, 9787878);
+$total_items = count($sxe->student);
+$rand_index = rand(0,($total_items-1));
+var_dump($rand_index);
+$sxe->student[$rand_index]->addChild($rand_name,$rand_value);
+var_dump($sxe->student[3]);
 var_dump($sxe);
