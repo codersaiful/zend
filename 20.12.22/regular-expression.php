ac0a9c68750d14361b0e9264b697a61b8371e2a4
Saiful Islam <codersaiful@gmail.com>
2022-12-20 20:50:25 +0600

    working

--- 20.12.22/regular-expression.php
+++ 20.12.22/regular-expression.php
@@ -1,1 +1,47 @@
 <?php 
+/**
+ * Regular Expression Pattern Special Sequence::
+ * \d - Matches any numeric number as [0-9]=>[\d]
+ * \D - Matches any non numeric number as [^0-9]=>[\D]
+ * \s - Matches any whitespace charecter[\t\n\r\f\n\v]=>[\s] Example: tab,new line,verticle space
+ * \S - Matches any non whitespace charecter[^\t\n\r\f\n\v]=>[\S] Example: tab,new line,verticle space
+ * \w - Matches any alpthanumeric charecter [a-zA-Z0-9_]=>[\w]
+ * \W - Matches any non alpthanumeric charecter [^a-zA-Z0-9_]=>[\W]
+ * 
+ * Modifiers:::
+ * i - Ignore Case, case nsensitive
+ * s - Include New line
+ * x - Extended for comments and whitespace
+ * m - Multiple Line
+ * b - Word Boundary
+ * B - Not word boundary
+ * A - Start of subject
+ * Z - End of subject
+ * z - End of subject
+ * G - First matching possition in subject
+ * 
+ * Meta Characters:
+ * ^- Start of Subject (or line and Miltple Mode)
+ * $ - End of subject
+ * [ - Start Character class defination
+ * ] - End Character class defination
+ * | - Alternates. eg matches a or b => (a|b)
+ * ( - Start subpattern
+ * ) - End subpattern
+ * \ - Escape character
+ * 
+ * Quantifier:
+ * n* - zero or more times
+ * n+ - One or more time
+ * n? - Zero or one time
+ * {n} - n occurence exactly
+ * {n,} - Atleast n occurence
+ * {n,m} - Between n and m occurrences
+ * 
+ * example:
+ * forum er link
+ * 
+ * @link https://www.php.net/manual/en/function.preg-match.php
+ * @link https://www.youtube.com/watch?v=5ckmCW8png0&t=1153s&ab_channel=LearnwithSumit-LWS-Bangladesh
+ */
+echo 'Regular Expression<br>';
