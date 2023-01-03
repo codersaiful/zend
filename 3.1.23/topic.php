4778c2b3e12728a2eefcd6e9e0525e316e69cff9
Saiful Islam <codersaiful@gmail.com>
2023-01-03 20:41:33 +0600

    working

---
+++ 3.1.23/topic.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * class hoyeche.. 
+ * 
+ * @author Saiful Islam <codersaiful@gmail.com>
+ * 
+ * LESSON:
+ * Database & SQL
+ * * SQL
+ * * Joins
+ * *Prepared Staements
+ * *Transaction
+ * *PDO
+ */
