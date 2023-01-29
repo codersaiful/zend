3bf9a467a0596a5652eeffcd06dae7900855f901
Saiful Islam <codersaiful@gmail.com>
2023-01-29 20:32:02 +0600

    practicing

---
+++ Mock Test/1 Mock Test/practice-mock-1-prac-2.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * After getting solution from Mashud vai
+ * 
+ * @date 29.1.2023
+ */
+var_dump( (bool) 5.8 );
+$__hello = 44;
+$x = 123 == 0123;
+
+var_dump($x);
+const saiful = 'Saiful';
