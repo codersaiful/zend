abc16568f5a7d4c5f4159e1fd3b19b5030832478
Saiful Islam <codersaiful@gmail.com>
2023-02-01 20:59:25 +0600

    practice done

---
+++ Mock Test/2 Mock Test/mock2-practice3.php
@@ -0,0 +1,20 @@
+<?php
+function hr($txt = ''){
+    echo "<hr>$txt";
+}
+$a = 8;
+$b = 7;
+var_dump("$a,$b",$a & $b, $a | $b, $a ^ $b);
+hr();
+$a = 2;
+$b = 2;
+var_dump("$a,$b",$a & $b, $a | $b, $a ^ $b);
+hr();
+$a = 11;
+$b = 2;
+var_dump("$a,$b",$a & $b, $a | $b, $a ^ $b);
+hr();
+$a = 15 << 2;
+$b = 10 >> 1;
+$c = 0xF0 >> 4;
+echo sprintf('%d - %d - 0x%X', $a, $b, $c);
